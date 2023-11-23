ee89585b62ad67882321c42068060dc1744cd33e
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-11-23 16:02:22 +0530

    google sign in and login with email password

--- lib/Modules/Utils/Helpers/Authentication_Helper/auth_helper.dart
+++ lib/Modules/Utils/Helpers/Authentication_Helper/auth_helper.dart
@@ -1,7 +1,9 @@
 import 'dart:ffi';
 
+import 'package:chat_app_3/Modules/Views/Login_Screen/Model/Sign_In_Model/sign_in_model.dart';
 import 'package:chat_app_3/Modules/Views/Login_Screen/Model/Sign_Up_Model/sign_up.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth_Helper {
   //TODO:1=>Private Constructor
@@ -11,6 +13,7 @@ class Auth_Helper {
   static final Auth_Helper auth_helper = Auth_Helper._();
 
   FirebaseAuth auth = FirebaseAuth.instance;
+  GoogleSignIn googleSignIn = GoogleSignIn();
 
   //TODO:Annoynimous Login
 
@@ -36,6 +39,46 @@ class Auth_Helper {
         password: data.password,
       );
 
+      res['user'] = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      res['error'] = e.code;
+    }
+    return res;
+  }
+
+  //TODO:SignIn
+
+  Future<Map<String, dynamic>> signIn({required SignIn data}) async {
+    Map<String, dynamic> res = {};
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: data.email, password: data.password);
+      res['user'] = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      res['error'] = e.code;
+    }
+    return res;
+  }
+
+  Future<Map<String, dynamic>> signInWithGoogle() async {
+    Map<String, dynamic> res = {};
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      UserCredential userCredential =
+          await auth.signInWithCredential(credential);
       res['user'] = userCredential.user;
     } on FirebaseAuthException catch (e) {
       res['error'] = e.code;
--- lib/Modules/Views/Login_Screen/Views/login_screen.dart
+++ lib/Modules/Views/Login_Screen/Views/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app_3/Modules/Utils/Helpers/Authentication_Helper/auth_helper.dart';
+import 'package:chat_app_3/Modules/Views/Login_Screen/Model/Sign_In_Model/sign_in_model.dart';
 import 'package:chat_app_3/Modules/Views/Login_Screen/Model/Sign_Up_Model/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,11 +7,16 @@ import 'package:get/get.dart';
 class Login_Screen extends StatelessWidget {
   Login_Screen({super.key});
   GlobalKey<FormState> signKey = GlobalKey<FormState>();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController signup_email_con = TextEditingController();
   TextEditingController signup_password_con = TextEditingController();
+  TextEditingController email_con = TextEditingController();
+  TextEditingController password_con = TextEditingController();
 
   String? signup_email;
   String? signup_password;
+  String? password;
+  String? email;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,213 +25,296 @@ class Login_Screen extends StatelessWidget {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    "https://assets-v2.lottiefiles.com/a/aa6a52de-1186-11ee-8908-df4910d4b75e/MHf9ngGhBS.gif",
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  filled: true,
-                  hintText: "enter email here..",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  filled: true,
-                  hintText: "enter password here..",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Ink(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
                 decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 5), color: Colors.grey, blurRadius: 5)
-                  ],
-                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://assets-v2.lottiefiles.com/a/aa6a52de-1186-11ee-8908-df4910d4b75e/MHf9ngGhBS.gif",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Container(
-                  height: 40,
-                  width: 180,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: email_con,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return "enter valid email";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (val) {
+                    email = val;
+                  },
+                  decoration: const InputDecoration(
+                    filled: true,
+                    hintText: "enter email here..",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: password_con,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return "enter valid password";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (val) {
+                    password = val;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    hintText: "enter password here..",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              InkWell(
+                onTap: () async {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+
+                    SignIn loginData =
+                        SignIn(email: email!, password: password!);
+
+                    Map<String, dynamic> res =
+                        await Auth_Helper.auth_helper.signIn(data: loginData);
+
+                    if (res['user'] != null) {
+                      Get.toNamed('/home');
+                    } else if (res['error'] != null) {
+                      Get.snackbar("Flutter Chat App", "Login Failed");
+                    }
+
+                    email_con.clear();
+                    password_con.clear();
+                  }
+                },
+                child: Ink(
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 5),
+                          color: Colors.grey,
+                          blurRadius: 5)
+                    ],
+                    color: Colors.blue,
+                  ),
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            InkWell(
-              onTap: () async {
-                Map? res = await Auth_Helper.auth_helper.AnnoynimousLogin();
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () async {
+                  Map? res = await Auth_Helper.auth_helper.AnnoynimousLogin();
 
-                if (res['user'] != null) {
-                  Get.toNamed('/home');
-                } else if (res['error'] != null) {
-                  Get.snackbar("Flutter Chat App", "Login Failed");
-                }
-              },
-              child: Ink(
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 5),
-                      color: Colors.grey,
-                      blurRadius: 5,
-                    )
-                  ],
-                  color: Colors.green,
-                ),
-                child: Container(
-                  height: 40,
-                  width: 180,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "Annoynimous Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                  if (res['user'] != null) {
+                    Get.toNamed('/home');
+                  } else if (res['error'] != null) {
+                    Get.snackbar("Flutter Chat App", "Login Failed");
+                  }
+                },
+                child: Ink(
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ],
+                    color: Colors.green,
+                  ),
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Annoynimous Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            InkWell(
-              onTap: () async {
-                Get.defaultDialog(
-                  title: "Chat App",
-                  content: Form(
-                    key: signKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          validator: (val) {
-                            if (val!.isEmpty) {
-                              return "Enter valid email";
-                            } else {
-                              return null;
-                            }
-                          },
-                          onSaved: (val) {
-                            signup_email = val;
-                          },
-                          controller: signup_email_con,
-                          decoration: InputDecoration(
-                            hintText: "enter email",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        TextFormField(
-                          validator: (val) {
-                            if (val!.isEmpty) {
-                              return "Enter valid password";
-                            } else {
-                              return null;
-                            }
-                          },
-                          onSaved: (val) {
-                            signup_password = val;
-                          },
-                          controller: signup_password_con,
-                          decoration: InputDecoration(
-                            hintText: "enter password",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        ElevatedButton(
-                          onPressed: () async {
-                            if (signKey.currentState!.validate()) {
-                              signKey.currentState!.save();
-
-                              SignUp signup_detail = SignUp(
-                                email: signup_email!,
-                                password: signup_password!,
-                              );
-
-                              Map<String, dynamic> res = await Auth_Helper
-                                  .auth_helper
-                                  .signUp(data: signup_detail);
-
-                              if (res['user'] != null) {
-                                // Get.snackbar("Chat App", "Signup Success");
-                                Get.back();
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () async {
+                  Get.defaultDialog(
+                    title: "Chat App",
+                    content: Form(
+                      key: signKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return "Enter valid email";
+                              } else {
+                                return null;
                               }
-                            }
+                            },
+                            onSaved: (val) {
+                              signup_email = val;
+                            },
+                            controller: signup_email_con,
+                            decoration: const InputDecoration(
+                              hintText: "enter email",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return "Enter valid password";
+                              } else {
+                                return null;
+                              }
+                            },
+                            onSaved: (val) {
+                              signup_password = val;
+                            },
+                            controller: signup_password_con,
+                            decoration: const InputDecoration(
+                              hintText: "enter password",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (signKey.currentState!.validate()) {
+                                signKey.currentState!.save();
 
-                            signup_email_con.clear();
-                            signup_password_con.clear();
-                          },
-                          child: Text("Sign Up"),
-                        ),
-                      ],
+                                SignUp signup_detail = SignUp(
+                                  email: signup_email!,
+                                  password: signup_password!,
+                                );
+
+                                Map<String, dynamic> res = await Auth_Helper
+                                    .auth_helper
+                                    .signUp(data: signup_detail);
+
+                                if (res['user'] != null) {
+                                  // Get.snackbar("Chat App", "Signup Success");
+                                  Get.back();
+                                }
+                              }
+
+                              signup_email_con.clear();
+                              signup_password_con.clear();
+                            },
+                            child: const Text("Sign Up"),
+                          ),
+                        ],
+                      ),
                     ),
+                  );
+                },
+                child: Ink(
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ],
+                    color: Colors.orange,
                   ),
-                );
-              },
-              child: Ink(
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 5),
-                      color: Colors.grey,
-                      blurRadius: 5,
-                    )
-                  ],
-                  color: Colors.orange,
-                ),
-                child: Container(
-                  height: 40,
-                  width: 180,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () async {
+                  Map res = await Auth_Helper.auth_helper.signInWithGoogle();
+                  if (res['user'] != null) {
+                    Get.toNamed('/home');
+                  } else if (res['error'] != null) {
+                    Get.snackbar("Flutter Chat App", "Login Failed");
+                  }
+                },
+                child: Ink(
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ],
+                    color: Colors.red,
+                  ),
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Continue With Google",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
