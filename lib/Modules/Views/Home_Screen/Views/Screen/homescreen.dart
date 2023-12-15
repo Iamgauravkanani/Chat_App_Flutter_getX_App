be6f9872fc39f33ebb1816339fe989a481eab115
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-12-15 10:47:05 +0530

    HomeScreen Restructured

--- lib/Modules/Views/Home_Screen/Views/Screen/homescreen.dart
+++ lib/Modules/Views/Home_Screen/Views/Screen/homescreen.dart
@@ -1,18 +1,36 @@
+import 'dart:developer';
+import 'dart:io';
 import 'package:chat_app_3/Modules/Views/Home_Screen/Controller/Tab_Controller/tab_controller.dart';
+import 'package:chat_app_3/Modules/Views/Home_Screen/Views/Components/Tabs/ChatTab/chat_tab.dart';
+import 'package:chat_app_3/Modules/Views/Home_Screen/Views/Components/Widgets/Drawer/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import '../../../../Utils/Helpers/Authentication_Helper/auth_helper.dart';
+import '../../Controller/Image_Picker_Controller/image_picker_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
+
   TabsController tabsController = Get.put(TabsController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: mydrawer(),
       appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("WhatsApp"),
         bottom: TabBar(
+          dividerColor: Colors.white,
+          labelColor: Colors.white,
+          indicatorColor: Colors.white,
           controller: tabsController.controller,
           tabs: tabsController.tabs,
         ),
+      ),
+      body: TabBarView(
+        controller: tabsController.controller,
+        children: [ChatTab(), ChatTab(), ChatTab()],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'dart:developer';
-
 import 'package:chat_app_3/Modules/Views/Chat_Screen/Views/chat_screen.dart';
 import 'package:chat_app_3/Modules/Views/Splash_Screen/Views/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Modules/Views/Home_Screen/Views/Screen/homescreen.dart';
-import 'Modules/Views/Home_Screen/Views/home_screen.dart';
 import 'Modules/Views/Login_Screen/Views/login.dart';
-
 import 'Modules/Views/Notification_Screen/Views/notification_screen.dart';
 import 'Modules/Views/Welcome_Screen/Views/welcome_screen.dart';
 import 'firebase_options.dart';
@@ -32,10 +29,10 @@ void main() async {
         useMaterial3: true,
       ),
       getPages: [
-        GetPage(name: '/', page: () => HomeScreen()),
+        GetPage(name: '/', page: () => SplashScreen()),
         GetPage(name: '/welcome', page: () => WelcomeScreen()),
         GetPage(name: '/login', page: () => Login()),
-        GetPage(name: '/home', page: () => Home_Screen()),
+        GetPage(name: '/home', page: () => HomeScreen()),
         GetPage(name: '/chat', page: () => Chat_Screen()),
         GetPage(name: '/notification', page: () => Notification_Screen()),
       ],
